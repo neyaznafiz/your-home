ce9ee76ae17c05c42acea3396818750a9f693139
niiyazobug <niiyaz5555@gmail.com>
2022-04-11 23:57:28 +0600

    route and navbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CheckOut from './Components/CheckOut/CheckOut';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Rooms from './Components/Rooms/Rooms';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/rooms' element={<Rooms></Rooms>}></Route>
+        <Route path='/checkout' element={<CheckOut></CheckOut>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      
+      </Routes>
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
+        <nav className='bg-gray-400 py-5 px-20'>
+
+            <div className='lg:flex md:flex lg:justify-between md:justify-between '>
+                <div>
+                    <h2 className='border-2 border-gray-500  py-1 px-3 rounded'>YOUR HOME</h2>
+                </div>
+
+                <div className=''>
+                    <Link className='ml-6' to='/'>Home</Link>
+                    <Link className='ml-6' to='/rooms'>Rooms</Link>
+                    <Link className='ml-6' to='/checkout'>Checkout</Link>
+                    <Link className='ml-6' to='/login'>Login</Link>
+                </div>
+            </div>
+
+        </nav>
     );
 };
 
