ad118dee6a429bd1a8093105b4c1796193c50197
niiyazobug <niiyaz5555@gmail.com>
2022-04-12 19:23:27 +0600

    coustom link added

---
+++ src/Components/CoustomLink/CoustomLink.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+const CoustomLink = ({ children, to, ...props }) => {
+
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+  
+
+    return (
+        <div>
+
+<Link
+        style={{ color: match ? "lightgray" : "black", textDecoration: match? "" : "none" }}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+     
+            
+        </div>
+    );
+};
+
+export default CoustomLink;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import auth from '../../Firebase/firebase.init';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import UseFirebase from '../UseFirebase/UseFirebase';
+import CoustomLink from '../CoustomLink/CoustomLink';
 
 const Header = () => {
 
@@ -17,16 +18,16 @@ const Header = () => {
                     <h2 className='border-2 border-gray-500  py-1 px-3 rounded'>YOUR HOME</h2>
                 </div>
 
-                <div className=''>
-                    <Link className='ml-6' to='/'>Home</Link>
-                    <Link className='ml-6' to='/rooms'>Rooms</Link>
-                    <Link className='ml-6' to='/checkout'>Checkout</Link>
+                <div className='flex'>
+                    <CoustomLink className='ml-6' to='/'>Home</CoustomLink>
+                    <CoustomLink className='ml-6' to='/rooms'>Rooms</CoustomLink>
+                    <CoustomLink className='ml-6' to='/checkout'>Checkout</CoustomLink>
 
                     {
                         user ?
-                            <Link to="/" className='ml-6' onClick={handleSingOut}>Logout</Link>
+                            <CoustomLink to="/" className='ml-6' onClick={handleSingOut}>Logout</CoustomLink>
                             :
-                        <Link className='ml-6' to='/login'>Login</Link>
+                        <CoustomLink className='ml-6' to='/login'>Login</CoustomLink>
                         }
                 </div>
             </div>
