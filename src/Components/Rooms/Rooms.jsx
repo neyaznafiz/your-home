060fb7bbd767d81a6fae6e83ef62746ca608cbd9
niiyazobug <niiyaz5555@gmail.com>
2022-04-12 17:05:47 +0600

    add to checkout button added

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -10,20 +10,23 @@ const Rooms = () => {
             .then(data => setRooms(data))
 
     }, [])
+
     return (
         <div className='grid grid-cols-3 gap-5 mx-10 my-10'>
 
             {
                 rooms.map(room => <div className=''>
 
-                    <div className='border w-60 rounded-md  mx-auto text-center'>
-                        <img src={room.img} alt="" className='w-60 h-60 rounded-md mx-auto' />
+                    <div className='border w-3/5 rounded-md  mx-auto text-center'>
+                        <img src={room.img} alt="" className=' h-60 rounded-t-md mx-auto' />
 
-                        <div className='p-4'>
-                            <h2>{room.name}</h2>
-                            <p>{room.price}</p>
+                        <div className='pt-4 bg-gray-100'>
+                            <h2 className='text-xl font-bold'>{room.name}</h2>
+                            <p className='font-semibold mb-3'>Price: ${room.price}</p>
                             <p>{room.info.slice(0, 100)}...</p>
+                            <button className='mx-auto border rounded-md my-5 py-2 px-4 text-xl text-gray800 font-semibold hover:bg-gray-300 hover:text-gray-700'>Add to checkout</button>
                         </div>
+
                     </div>
 
                 </div>)
