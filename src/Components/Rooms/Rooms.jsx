75e591eb2a82a44eb742bd150b0ef8fe286a7f43
niiyazobug <niiyaz5555@gmail.com>
2022-04-12 16:55:02 +0600

    added room fake more data and drid room cart

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -11,21 +11,18 @@ const Rooms = () => {
 
     }, [])
     return (
-        <div>
+        <div className='grid grid-cols-3 gap-5 mx-10 my-10'>
 
             {
                 rooms.map(room => <div className=''>
 
+                    <div className='border w-60 rounded-md  mx-auto text-center'>
+                        <img src={room.img} alt="" className='w-60 h-60 rounded-md mx-auto' />
 
-                    <div className=''>
-                        <div className='border  rounded-md w-3/12 mx-auto text-center'>
-                            <img src={room.img} alt="" className='w-screen rounded-md' />
-                            
-                            <div className='p-4'>
+                        <div className='p-4'>
                             <h2>{room.name}</h2>
                             <p>{room.price}</p>
-                            <p>{room.info}</p>
-                            </div>
+                            <p>{room.info.slice(0, 100)}...</p>
                         </div>
                     </div>
 
